cd7397f1fecf22f9d756b9a1535ebea984d0af61
Thankan-Chettan-84 <116328358+Thankan-Chettan-84@users.noreply.github.com>
2025-05-04 14:56:30 +0530

    Update bot.py
    Customized

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@
 # License Link : https://github.com/PR0FESS0R-99/Auto-Approved-Bot/blob/Auto-Approved-Bot/LICENSE
 
 from os import environ
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums, errors
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
 
 pr0fess0r_99=Client(
@@ -36,25 +36,42 @@ pr0fess0r_99=Client(
 )
 
 CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).split()]
-TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
+#TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
+TEXT = """<b>Hello {mention},
+
+<blockquote>Yᴏᴜʀ Rᴇqᴜᴇꜱᴛ Tᴏ Jᴏɪɴ {title} Hᴀꜱ Bᴇᴇɴ Aᴩᴩʀᴏᴠᴇᴅ 🔥</blockquote>
+
+Send /start to know more.</b>"""
+
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 @pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
 async def start(client: pr0fess0r_99, message: Message):
-    approvedbot = await client.get_me() 
-    button = [[ InlineKeyboardButton("📦 Repo", url="https://github.com/PR0FESS0R-99/Auto-Approved-Bot"), InlineKeyboardButton("Updates 📢", url="t.me/Mo_Tech_YT") ],
-              [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
-    await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+    #approvedbot = await client.get_me() 
+    buttons = [[
+                InlineKeyboardButton('♻️ GROUP', url='https://t.me/MovieJunctionGrp'),
+                InlineKeyboardButton('CHANNELS 🏷️', url='https://t.me/Mj_Linkz/1318')
+         ]]
+    await client.send_message(chat_id=message.chat.id, text=f"<b>Hᴇʟʟᴏ {message.from_user.mention}!\n\n<blockquote>🤖 I'ᴀᴍ Aɴ Aᴜᴛᴏ Aᴩᴩʀᴏᴠᴀʟ Bᴏᴛ 💥</blockquote>\n\n⚠️ 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 & 𝗖𝗵𝗮𝗻𝗻𝗲𝗹𝘀 𝘁𝗼 𝗞𝗻𝗼𝘄 𝗠𝗼𝗿𝗲 👇", reply_markup=InlineKeyboardMarkup(button), parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
 
 @pr0fess0r_99.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
-    print(f"{user.first_name} Joined 🤝") # Logs
-    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
-    #   print("Welcome....")
+    try:       
+         await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+         print(f"{user.first_name} Joined {chat.title} 🤝") # Logs
+         ##if APPROVED == "on":
+         buttons = [[
+                InlineKeyboardButton('♻️ GROUP', url='https://t.me/MovieJunctionGrp'),
+                InlineKeyboardButton('CHANNELS 🏷️', url='https://t.me/Mj_Linkz/1318')
+         ]]
+         reply_markup = InlineKeyboardMarkup(buttons)    
+         await client.send_message(chat_id=user.id, text=TEXT.format(mention=user.mention, title=chat.title), reply_markup=InlineKeyboardMarkup(buttons), parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
+     except errors.PeerIdInvalid as e:
+         print("user isn't start bot(means group)")
+     except Exception as err:
+        print(str(err))    
 
 print("Auto Approved Bot")
 pr0fess0r_99.run()
